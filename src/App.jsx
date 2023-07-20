8aad5f18f1db7a3a9ecc6df0d9e6a61d3df8d2b7
Toxe <cerc-12@hotmail.com>
2023-07-20 09:03:57 -0400

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HomePage } from './routes/HomePage';
 
 function App() {
   return (
     <>
-      <h1 className='text-4xl'>Hello, World</h1>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          {/* <Route path='/category' element={<TrendingPage />}/>
+          <Route path='/category' element={<SearchPage />}/>
+          <Route path='/category' element={<MoviePage />}/>
+          <Route path='/category' element={<CategoryPage />}/> */}
+          <Route path='*' element={<p>Not Found</p>} />
+        </Routes>
+      </HashRouter>
     </>
   )
 }
