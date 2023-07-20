4911d26684965846b20068740c1b723a5b7aff80
Toxe <cerc-12@hotmail.com>
2023-07-20 06:20:16 -0400

    testing deploy with gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/movieapp-react/'
+  base: '/movieapp-react/',
   plugins: [react()],
 })
