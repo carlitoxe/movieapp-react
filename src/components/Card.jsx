a76da016b3be4e95c775ced379ef9f322fe70361
Toxe <cerc-12@hotmail.com>
2023-07-31 00:31:06 -0400

    Minor change to UI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -25,12 +25,12 @@ function Card({movie, loading, img, movies, lastMovieElementRef, i}) {
             data-src={movie.poster_path ? `https://image.tmdb.org/t/p/w300${movie.poster_path}` : noimage} 
             alt={`${movie.title} poster`} 
             ref={imgRef}
-            className="movie-img md:hover:scale-110 transition duration-500 w-[162px] md:mb-2 md:w-[198px] md:min-w-[198px] md:h-[300px] md:min-h-[300px] object-cover rounded-lg cursor-pointer"
+            className="movie-img md:hover:scale-110 transition duration-500 w-[162px] h-[240px] min-h-[240px] md:mb-2 md:w-[198px] md:min-w-[198px] md:h-[300px] md:min-h-[300px] object-cover rounded-lg cursor-pointer"
             onClick={() => navigate(`/movie/${movie.id}`)}
         />
   
         <p className="mt-2 px-2"><span className="text-yellow-300">★</span> {movie.vote_average.toFixed(1)}</p>
-        <a className="cursor-pointer px-2 pb-1 hover:text-blue-700 mt-0.5" onClick={() => navigate(`/movie/${movie.id}`)}>{movie.title}</a>
+        <a className="cursor-pointer px-2 pb-1 hover:text-blue-700 mt-0.5 mb-1" onClick={() => navigate(`/movie/${movie.id}`)}>{movie.title}</a>
     </div>
 
         
