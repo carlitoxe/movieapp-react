93933228475dfcd1be3db95fd90b2edd4729a3dd
Toxe <cerc-12@hotmail.com>
2023-07-31 04:04:43 -0400

    Upgrade search form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -63,7 +63,7 @@ function SearchForm({value, setValue}) {
     <button
   type="submit"
   onClick={() => setValue(search)}
-  className="hidden absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-full border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-full border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 >
   <svg
     className="w-4 h-4"
