6a93011ac8aa52daa6c504c5c037ec902a11482b
Toxe <cerc-12@hotmail.com>
2023-07-31 13:07:29 -0400

    Minor change in search form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -54,14 +54,14 @@ function SearchForm({value, setValue}) {
     <input
       value={search} 
       onChange={e => setSearch(e.target.value.toLowerCase())}
-      className="block w-full px-8 py-1.5 pl-5 border rounded-full bg-transparent border-gray-600  placeholder-gray-500  text-white focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+      className="block w-full px-8 py-1.5 pl-5 border rounded-full bg-transparent border-gray-700  placeholder-gray-500  text-white focus:outline-none focus:border-blue-700 focus:ring-1 focus:ring-blue-700"
       placeholder="Search for movies..."
      
     />
     <button
   type="submit"
   onClick={() => setValue(search)}
-  className="absolute top-0 right-[0.5px] p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-full border border-blue-700 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-600 rounded-r-full border border-blue-700 hover:bg-blue-800 focus:ring-1 focus:outline-none  hover:bg-blue-800 focus:ring-blue-800"
 >
   <svg
     className="w-4 h-4"
