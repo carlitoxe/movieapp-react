1361fdf3397b81fe7a2c7bab6a84059bdb016e6a
Toxe <cerc-12@hotmail.com>
2023-07-31 04:20:37 -0400

    Upgrade search form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -26,7 +26,7 @@ function SearchForm({value, setValue}) {
     //         </button>
     //     </form>
     // </div>
-    <form onSubmit={onSubmit} className="px-6 mt-4 md:mt-0 md:px-0">
+    <form onSubmit={onSubmit} className="px-8 mt-4 md:mt-0 md:px-0">
   {/* <label
     htmlFor="default-search"
     className="mb-2 text-sm font-medium sr-only text-white"
@@ -34,7 +34,7 @@ function SearchForm({value, setValue}) {
     Search
   </label> */}
   <div className="relative">
-    <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
+    {/* <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
       <svg
         className="w-4 h-4 text-gray-400"
         aria-hidden="true"
@@ -50,20 +50,18 @@ function SearchForm({value, setValue}) {
           d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
         />
       </svg>
-    </div>
+    </div> */}
     <input
       value={search} 
       onChange={e => setSearch(e.target.value.toLowerCase())}
-      type="search"
-      id="default-search"
-      className="block w-full px-4 py-1.5 pl-10 border rounded-full bg-transparent border-gray-600  placeholder-gray-500  text-white focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+      className="block w-full px-8 py-1.5 pl-5 border rounded-full bg-transparent border-gray-600  placeholder-gray-500  text-white focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
       placeholder="Search for movies..."
      
     />
     <button
   type="submit"
   onClick={() => setValue(search)}
-  className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-full border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  className="absolute top-0 right-[0.5px] p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-full border border-blue-700 hover:bg-blue-800 focus:ring-1 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 >
   <svg
     className="w-4 h-4"
