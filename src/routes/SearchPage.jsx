c141c44f297b6a8553bc48bbc3f1768639f04c5b
Toxe <cerc-12@hotmail.com>
2023-07-31 01:02:50 -0400

    Upgrade responsive UI

--- src/routes/SearchPage.jsx
+++ src/routes/SearchPage.jsx
@@ -35,16 +35,17 @@ function SearchPage() {
         />
                 <HeaderRight />
             </Header>
-            {!loadingMovies && movies.length > 0 ? 
+            
+            {movies.length > 0 ? 
             <>
-            <h1 className="text-center font-medium text-2xl text-white mt-6 md:mt-10 mb-4 md:mb-8">Search for '{query}'</h1>
+            <h1 className="text-center font-medium text-2xl text-white mt-6 md:mt-10 mb-4 md:mb-8">Search for <span className="font-bold italic">"{query}"</span></h1>
                 <RenderMovies 
                     movies={movies}
                     loading={loadingMovies}
                     lastMovieElementRef={lastMovieElementRef}
                 />
                 </>
-                : <div className="text-center font-medium text-2xl text-white md:mt-10 mb-8">No Results for '{query}'</div>
+                : <div className="text-center font-medium text-2xl text-white md:mt-10 mb-8">No Results for "{query}"</div>
             }
 
         </>
