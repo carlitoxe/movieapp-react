bcb2f3b0493915dc2d9c6bfe59f062cbd3c4c189
Toxe <cerc-12@hotmail.com>
2023-07-30 21:21:43 -0400

    Add responsive to actor card (cast list)

--- src/components/CardActor.jsx
+++ src/components/CardActor.jsx
@@ -14,16 +14,16 @@ function CardActor({actor, img}) {
     }, [img])
 
     return (
-        <li className='min-w-[140px] w-[140px] shadow-md shadow-slate-700 my-3 ml-1' key={actor.id}>
+        <li className='w-[120px] md:min-w-[140px] md:w-[140px] shadow-md shadow-slate-700 my-3 ml-1' key={actor.id}>
             <img
-                className="max-w-[138px] min-h-[175px] max-h-[175px] rounded-t-lg"
+                className="min-w-[120px] min-h-[133px] md:max-w-[138px] md:min-h-[175px] md:max-h-[175px] rounded-t-lg"
                 data-src={actor.profile_path ? `https://www.themoviedb.org/t/p/w138_and_h175_face${actor.profile_path}` : noimage} 
             ref={imgRef}
         
                 alt={`${actor.name} picture`} 
             />
-            <p className="mt-3 px-3 text-white font-semibold">{actor.name}</p>
-            <p className="px-3 mt-1 pb-1 text-[0.9rem]">{actor.character}</p>
+            <p className="mt-2 px-2 md:mt-3 md:px-3 text-sm md:text-base text-white font-semibold">{actor.name}</p>
+            <p className="px-2 md:px-3 md:mt-1 pb-1 text-sm md:text-[0.9rem]">{actor.character}</p>
         </li>
     )
 }
