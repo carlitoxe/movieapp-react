ae1a029e3d15f63ea7f96996ad482ac8068f2a07
Toxe <cerc-12@hotmail.com>
2023-07-31 01:41:18 -0400

    Change in UI

--- src/components/BackgroundGradient.jsx
+++ src/components/BackgroundGradient.jsx
@@ -1,6 +1,6 @@
 function BackgroundGradient({children, colors, isPhone}) {
     const gradientDirection = isPhone ? 'bottom' : 'right';
-    const percentage = isPhone ? '30%' : '20%'
+    const percentage = isPhone ? '40%' : '20%'
 
     return (
         <div className="background-gradient pl-0 md:backdrop-blur-[2px] md:flex pt-6 pb-8 md:pb-44 md:pb-[3.2rem] md:pl-3" 
